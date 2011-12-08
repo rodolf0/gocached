87acc18378dbef1456503ac130d3d042daf53e5d
Rafael Darder <darder@gmail.com>
2011-12-08 14:42:39 -0300

    heap documentation + gofmt -s -w on new files

--- expiry/heap.go
+++ expiry/heap.go
@@ -1,39 +1,53 @@
 package expiry
 
+//This is the node for the expiry heap. We just store the absolute expiry time and a pointer to the element key.
+//Note that an entry on the heap does not guarantee that there's an entry on the storage map or that the map entry actually has expired. 
+//The invariant is:
+// Each entry in the storage map has at least one entry with the same key and ttl in the queue, or it's in the expiry channel buffer. This holds for the 'current' entry value at any given time, including updates
+
 type Entry struct {
 	Exptime uint32
 	Key     *string
 }
 
+// The heap is implemented in an array, hence the type is an alias of Entry slice.
+// Note that as we want to support extensible heap capacities, functions like Push and Pop may change the underlying array (Push), or re-slice (Pop). In either case, we need a slice pointer, to be able to update it as a side-effect.
 type Heap []Entry
 
+//make a heap of start_size and return its pointer
 func NewHeap(start_size int) *Heap {
 	r := make(Heap, 0, start_size)
-  return &r
+	return &r
 }
 
+//heap.Interface Lean() implementation
 func (h Heap) Len() int {
 	return len(h)
 }
 
+//heap.Interface Less() implementation 
+// here we could also take into consideration the key as a tie resolver, but it's not significant right now, as we don't care which of two entries with same exptime we want upper in the heap
 func (h Heap) Less(i, j int) bool {
-  return h[i].Exptime < h[j].Exptime
+	return h[i].Exptime <= h[j].Exptime
 }
 
+// heap.Interface Swap implementation
 func (h Heap) Swap(i, j int) {
 	h[i].Exptime, h[j].Exptime = h[j].Exptime, h[i].Exptime
 	h[i].Key, h[j].Key = h[j].Key, h[i].Key
 }
 
+// heap.Interface Push implementation. Semantic is 'append'. 
+// Note that Push and Pop are implemented upon heap pointers, where Swap, Less and Len don't need pointers and are implemented on values. Recall 'the method set of the pointer type includes the method set of the value type'. May change the underlying array (and the slice)
 func (h *Heap) Push(ientry interface{}) {
 	entry := ientry.(Entry)
 	*h = append(*h, entry)
 }
 
+// heap.Interface Pop implementation. Removes the last element on the heap-array. Changes the slice
 func (h *Heap) Pop() interface{} {
 	last := len(*h) - 1
 	ret := (*h)[last]
 	*h = (*h)[:last]
 	return ret
 }
-
--- expiry/heap_test.go
+++ expiry/heap_test.go
@@ -2,37 +2,36 @@ package expiry
 
 import (
 	"container/heap"
-  "testing"
+	"testing"
 )
 
-
 func TestPushPopNoExpand(t *testing.T) {
-  h := NewHeap(3)
-  heap.Push(h, Entry{10,nil})
-  heap.Push(h, Entry{1,nil})
-  heap.Push(h, Entry{5,nil})
+	h := NewHeap(3)
+	heap.Push(h, Entry{10, nil})
+	heap.Push(h, Entry{1, nil})
+	heap.Push(h, Entry{5, nil})
 
-  for _, expected := range []uint32{1,5,10} {
-    v := heap.Pop(h).(Entry).Exptime
-    if v != expected {
-      t.Error("Not in order, expected", expected, "got", v)
-    }
-  }
+	for _, expected := range []uint32{1, 5, 10} {
+		v := heap.Pop(h).(Entry).Exptime
+		if v != expected {
+			t.Error("Not in order, expected", expected, "got", v)
+		}
+	}
 }
 
 func TestPushPopExpand(t *testing.T) {
-  h := NewHeap(3)
-  heap.Push(h, Entry{10,nil})
-  heap.Push(h, Entry{1,nil})
-  heap.Push(h, Entry{5,nil})
-  heap.Push(h, Entry{50,nil})
-  heap.Push(h, Entry{72,nil})
-  heap.Push(h, Entry{17,nil})
+	h := NewHeap(3)
+	heap.Push(h, Entry{10, nil})
+	heap.Push(h, Entry{1, nil})
+	heap.Push(h, Entry{5, nil})
+	heap.Push(h, Entry{50, nil})
+	heap.Push(h, Entry{72, nil})
+	heap.Push(h, Entry{17, nil})
 
-  for _, expected := range []uint32{1,5,10,17,50,72} {
-    v := heap.Pop(h).(Entry).Exptime
-    if v != expected {
-      t.Error("Not in order, expected", expected, "got", v)
-    }
-  }
+	for _, expected := range []uint32{1, 5, 10, 17, 50, 72} {
+		v := heap.Pop(h).(Entry).Exptime
+		if v != expected {
+			t.Error("Not in order, expected", expected, "got", v)
+		}
+	}
 }
