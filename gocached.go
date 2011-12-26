3a81e6455d992d3c1c655283bbca698f79b87dbb
Rafael Darder <darder@gmail.com>
2011-12-26 19:40:06 -0300

    add storage implementation command line flag, make flags get actually parsed. add frequency flag for notifystorage, fix sessions, no longer building a new storage on each connection rebase into rodolfo, restore clientHanler goroutine

--- gocached.go
+++ gocached.go
@@ -1,37 +1,72 @@
 package main
 
 import (
-  "flag"
-  "os"
+	"flag"
+	"os"
 	"log"
-  "net"
+	"net"
 )
 
-var logger = log.New(os.Stdout, "gocached: ", log.Lshortfile | log.LstdFlags)
-var port = flag.String("port", "11212", "memcached port")
+//global logger
+var logger = log.New(os.Stdout, "gocached: ", log.Lshortfile|log.LstdFlags)
 
 
 func main() {
-  newGenerationalStorage()
-  if addr, err := net.ResolveTCPAddr("tcp", "0.0.0.0:" + *port); err != nil {
-    logger.Fatalf("Unable to resolv local port %s\n", *port)
-  } else if listener, err := net.ListenTCP("tcp", addr); err != nil {
-    logger.Fatalln("Unable to listen on requested port")
-  } else {
+	var storage Storage
+	var factory StorageFactory
+
+	// command line flags and parsing
+	var port = flag.String("port", "11212", "memcached port")
+	var storage_choice = flag.String("storage", "generational",
+		"storage implementation (generational, heap, map)")
+	var expiring_frequency = flag.Int64("expiring-interval", 10,
+		"expiring interval in seconds")
+	var partitions = flag.Int("partitions", 10, "storage partitions (0 or 1 to disable)")
+	flag.Parse()
+
+	// storage implementation selection
+	switch *storage_choice {
+	case "map":
+		logger.Print("warning, map storage does not expire entries")
+		factory = func () Storage { return newMapStorage() }
+	case "generational":
+		factory = func () Storage { return newGenerationalStorage() }
+	case "heap":
+		factory = func () Storage { return newNotifyStorage(*expiring_frequency) }
+	default:
+		logger.Fatalln("Invalid storage selection")
+	}
+
+	// whether using partitioned or standalone storage
+	if *partitions > 1 {
+		storage = newHashingStorage(uint32(*partitions), factory)
+	} else {
+		storage = factory()
+	}
+
+	// network setup
+	if addr, err := net.ResolveTCPAddr("tcp", "0.0.0.0:"+*port); err != nil {
+		logger.Fatalf("Unable to resolv local port %s\n", *port)
+	} else if listener, err := net.ListenTCP("tcp", addr); err != nil {
+		logger.Fatalln("Unable to listen on requested port")
+	} else {
+    // server loop
     logger.Printf("Starting Gocached server")
     for {
       if conn, err := listener.AcceptTCP(); err != nil {
         logger.Println("An error ocurred accepting a new connection")
       } else {
-        go func(connection *net.TCPConn) {
-          defer connection.Close()
-          if session, err := NewSession(connection); err != nil {
-            logger.Println("An error ocurred creating a new session")
-          } else {
-            session.CommandLoop()
-          }
-        }(conn)
+        go clientHandler(conn, storage)
       }
     }
   }
+}
+
+func clientHandler(conn *net.TCPConn, store Storage) {
+	defer conn.Close()
+	if session, err := NewSession(conn, store); err != nil {
+		logger.Println("An error ocurred creating a new session")
+	} else {
+    session.CommandLoop()
+	}
 }
